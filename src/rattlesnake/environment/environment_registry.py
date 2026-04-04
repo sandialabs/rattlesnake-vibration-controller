4aad967deed8144e24024142485f01183a7a2ab4
CodyLangston34135 <Cody.Langston75@gmail.com>
2026-04-04 15:52:28 -0400

    Added unimplemented lists to registry which doesnt load hardware/environments that have not been implemented yet. Will be used for both unit tests and user interface

--- src/rattlesnake/environment/environment_registry.py
+++ src/rattlesnake/environment/environment_registry.py
@@ -1,64 +1,84 @@
 from rattlesnake.environment.environment_utilities import EnvironmentType
-from rattlesnake.environment.transient_sys_id_environment import (
-    TransientCommands,
-    TransientMetadata,
-    TransientEnvironment,
-    transient_process,
-)
-from rattlesnake.environment.sine_sys_id_environment import (
-    SineCommands,
-    SineMetadata,
-    SineEnvironment,
-    sine_process,
-)
-from rattlesnake.environment.modal_environment import (
-    ModalCommands,
-    ModalMetadata,
-    ModalEnvironment,
-    modal_process,
-)
-from rattlesnake.environment.random_vibration_sys_id_environment import (
-    RandomVibrationCommands,
-    RandomVibrationMetadata,
-    RandomVibrationEnvironment,
-    random_vibration_process,
-)
-from rattlesnake.environment.time_environment import (
-    TimeParameters,
-    TimeEnvironment,
-    time_process,
-)
+
+UNIMPLEMENTED_ENVIRONMENT = [
+    EnvironmentType.TIME,
+    EnvironmentType.MODAL,
+    EnvironmentType.SINE,
+    EnvironmentType.TRANSIENT,
+    EnvironmentType.RANDOM,
+]
 
 ENVIRONMENT_COMMANDS = {}
 ENVIRONMENT_METADATA = {}
 ENVIRONMENT_CLASS = {}
 ENVIRONMENT_PROCESS = {}
 
-# Time Environment
-ENVIRONMENT_METADATA[EnvironmentType.TIME] = TimeParameters
-ENVIRONMENT_CLASS[EnvironmentType.TIME] = TimeEnvironment
-ENVIRONMENT_PROCESS[EnvironmentType.TIME] = time_process
+for environment_type in EnvironmentType:
+    if environment_type in UNIMPLEMENTED_ENVIRONMENT:
+        continue
+    match environment_type:
+        case EnvironmentType.TIME:
+            from rattlesnake.environment.time_environment import (
+                TimeParameters,
+                TimeEnvironment,
+                time_process,
+            )
 
-# Modal Environment
-ENVIRONMENT_COMMANDS[EnvironmentType.MODAL] = ModalCommands
-ENVIRONMENT_METADATA[EnvironmentType.MODAL] = ModalMetadata
-ENVIRONMENT_CLASS[EnvironmentType.MODAL] = ModalEnvironment
-ENVIRONMENT_PROCESS[EnvironmentType.MODAL] = modal_process
+            ENVIRONMENT_METADATA[EnvironmentType.TIME] = TimeParameters
+            ENVIRONMENT_CLASS[EnvironmentType.TIME] = TimeEnvironment
+            ENVIRONMENT_PROCESS[EnvironmentType.TIME] = time_process
 
-# Sine Environment
-ENVIRONMENT_COMMANDS[EnvironmentType.SINE] = SineCommands
-ENVIRONMENT_METADATA[EnvironmentType.SINE] = SineMetadata
-ENVIRONMENT_CLASS[EnvironmentType.SINE] = SineEnvironment
-ENVIRONMENT_PROCESS[EnvironmentType.SINE] = sine_process
+        case EnvironmentType.MODAL:
+            from rattlesnake.environment.modal_environment import (
+                ModalCommands,
+                ModalMetadata,
+                ModalEnvironment,
+                modal_process,
+            )
 
-# Transient Environment
-ENVIRONMENT_COMMANDS[EnvironmentType.TRANSIENT] = TransientCommands
-ENVIRONMENT_METADATA[EnvironmentType.TRANSIENT] = TransientMetadata
-ENVIRONMENT_CLASS[EnvironmentType.TRANSIENT] = TransientEnvironment
-ENVIRONMENT_PROCESS[EnvironmentType.TRANSIENT] = transient_process
+            ENVIRONMENT_COMMANDS[EnvironmentType.MODAL] = ModalCommands
+            ENVIRONMENT_METADATA[EnvironmentType.MODAL] = ModalMetadata
+            ENVIRONMENT_CLASS[EnvironmentType.MODAL] = ModalEnvironment
+            ENVIRONMENT_PROCESS[EnvironmentType.MODAL] = modal_process
 
-# Random Environment
-ENVIRONMENT_COMMANDS[EnvironmentType.RANDOM] = RandomVibrationCommands
-ENVIRONMENT_METADATA[EnvironmentType.RANDOM] = RandomVibrationMetadata
-ENVIRONMENT_CLASS[EnvironmentType.RANDOM] = RandomVibrationEnvironment
-ENVIRONMENT_PROCESS[EnvironmentType.RANDOM] = random_vibration_process
+        case EnvironmentType.SINE:
+            from rattlesnake.environment.sine_sys_id_environment import (
+                SineCommands,
+                SineMetadata,
+                SineEnvironment,
+                sine_process,
+            )
+
+            ENVIRONMENT_COMMANDS[EnvironmentType.SINE] = SineCommands
+            ENVIRONMENT_METADATA[EnvironmentType.SINE] = SineMetadata
+            ENVIRONMENT_CLASS[EnvironmentType.SINE] = SineEnvironment
+            ENVIRONMENT_PROCESS[EnvironmentType.SINE] = sine_process
+
+        case EnvironmentType.TRANSIENT:
+            from rattlesnake.environment.transient_sys_id_environment import (
+                TransientCommands,
+                TransientMetadata,
+                TransientEnvironment,
+                transient_process,
+            )
+
+            ENVIRONMENT_COMMANDS[EnvironmentType.TRANSIENT] = TransientCommands
+            ENVIRONMENT_METADATA[EnvironmentType.TRANSIENT] = TransientMetadata
+            ENVIRONMENT_CLASS[EnvironmentType.TRANSIENT] = TransientEnvironment
+            ENVIRONMENT_PROCESS[EnvironmentType.TRANSIENT] = transient_process
+
+        case EnvironmentType.RANDOM:
+            from rattlesnake.environment.random_vibration_sys_id_environment import (
+                RandomVibrationCommands,
+                RandomVibrationMetadata,
+                RandomVibrationEnvironment,
+                random_vibration_process,
+            )
+
+            ENVIRONMENT_COMMANDS[EnvironmentType.RANDOM] = RandomVibrationCommands
+            ENVIRONMENT_METADATA[EnvironmentType.RANDOM] = RandomVibrationMetadata
+            ENVIRONMENT_CLASS[EnvironmentType.RANDOM] = RandomVibrationEnvironment
+            ENVIRONMENT_PROCESS[EnvironmentType.RANDOM] = random_vibration_process
+
+        case _:
+            continue
