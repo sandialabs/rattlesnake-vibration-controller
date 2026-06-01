b62f2b11ddf0946fc7aa0795325deb368eed85ea
CodyLangston34135 <Cody.Langston75@gmail.com>
2026-06-01 13:17:47 -0600

    Moved tests into test/short. Added assert False test.

--- tests/dev_tests/est_sdynpy_system_virtual_hardware_old.py
+++
@@ -1,7 +0,0 @@
-from rattlesnake.hardware.sdynpy_system_virtual_hardware_virtual_hardware import (
-    SDynPySystemAcquisition,
-)
-from unittest import mock
-import multiprocessing as mp
-import pytest
-import numpy as np
