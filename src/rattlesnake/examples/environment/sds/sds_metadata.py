f333591193409c1da3825e31e9b5840e65871b30
CodyLangston34135 <Cody.Langston75@gmail.com>
2026-08-11 13:38:31 -0600

    Added sds example file.

---
+++ src/rattlesnake/examples/environment/sds/sds_metadata.py
@@ -0,0 +1,36 @@
+import numpy as np
+import netCDF4 as nc4
+import openpyxl
+
+import rattlesnake.examples.defaults as defaults
+from rattlesnake.utilities import GlobalCommands
+from rattlesnake.load_utilities import load_profile_from_workbook
+from rattlesnake.environment.sds_sys_id_metadata import SDSMetadata
+from rattlesnake.environment.sds_sys_id_environment import SDSEnvironment
+from rattlesnake.environment.sds_sys_id_utilities import SDSInstructions
+from rattlesnake.profile_manager import ProfileEvent
+from rattlesnake.environment.environment_utilities import EnvironmentType
+
+
+def manual_sds_metadata():
+    pass
+
+
+def netcdf_sds_metadata():
+    pass
+
+
+def worksheet_sds_metadata():
+    pass
+
+
+def sds_instructions():
+    pass
+
+
+def sds_event_list():
+    pass
+
+
+def worksheet_sds_event_list():
+    pass
